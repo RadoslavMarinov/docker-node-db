9ad455e6c70ff22c3c16098bb77a3e95f1e7a67b
radsolav-marinov <radoslav.k.marinov@gmail.com>
2023-08-08 21:51:06 +0300

    Create append csv function

--- app/utils/csv/fast-csv.utils.ts
+++ app/utils/csv/fast-csv.utils.ts
@@ -1,24 +1,46 @@
 import { write } from "fast-csv";
-import fs, { createWriteStream } from "fs";
+import { existsSync, createWriteStream } from "fs";
+import path from "path";
+
+async function appendToCsv(
+  filePath: string,
+  data: Array<Record<string, any>>
+) {
+  let writeHeader = false;
+  if (!existsSync(filePath)) {
+    console.log(`FILE EXIST NOT EXISTS `, filePath)
+    // throw new Error(`The file '${filePath}' dos not exists!`);
+    writeHeader = true
+  }
+  console.log(`WRITE headers `, writeHeader)
+
+  const writeStream = createWriteStream(filePath, { flags: "a" });
+  return new Promise((resolve, reject) => {
+    write(data, { headers: writeHeader, includeEndRowDelimiter: true })
+      .pipe(writeStream)
+      .on("finish", () => {
+        console.log(`DATA APPENDED !`);
+        resolve(true);
+      });
+  });
+}
 
 (async function () {
-  const ws = createWriteStream("fast.csv", { flags: "a" });
   const data = [
     {
       name: "Riko",
-      age: 30,
+      age: 30, 
     },
     {
       name: "Sonya",
-      age: 35,
+      age: 35
     },
     {
       name: "Gaby",
       age: 8,
     },
   ];
-  write(data, { headers: false, includeEndRowDelimiter: true })
-    .pipe(ws)
-    .on("close", () => console.log(`Finish`))
-    .on("close", () => console.log(`close`));
+ 
+  await appendToCsv(path.resolve(__dirname, "csv.csv"), data);
+  console.log(`DDDDOOONNEEE`)
 })();
